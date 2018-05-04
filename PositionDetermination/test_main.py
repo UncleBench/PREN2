2c0ac70cfae5ade3b170ed89c1c99d9e77240cb0
Aron <schmied.aron@gmail.com>
2018-05-04 09:12:29 +0200

    file to test the MotorControl, PosSensor and SerialCommunication class

---
+++ PositionDetermination/test_main.py
@@ -0,0 +1,26 @@
+from MotorControl import MotorControl
+from Communication import SerialCommunication
+from PositionDetermination import PosSensor
+import time
+
+if __name__ == '__main__':
+    arduino = SerialCommunication('/dev/ttyACM1', 9600)
+    mc = MotorControl(0, 0, com='/dev/ttyACM0')
+    posSensor = PosSensor()
+
+    alpha_ = 2000
+    beta_ = 2000
+
+    mc.drive_x(20, 5000)
+    while 1:
+        time.sleep(0.2)
+        try:
+            alpha_ = arduino.getRawAlpha()
+            beta_ = arduino.getRawBeta()
+        except:
+            pass
+        dist = mc.get_distance_driven()(0)
+        pos = posSensor.get_pos_prachtstueck(alpha_, beta_, dist)
+        print('x:{:5.1f}  z:{:5.1f} | s:{:5.1f} | ralpha:{:4d} rbeta:{:4d}'.format(pos.x, pos.y, dist, alpha_, beta_))
+
+
