633ddfab68749533e4ebc7eecb3f90ef55f27fe5
Aron <schmied.aron@gmail.com>
2018-05-08 17:16:56 +0200

    changed to version without motor control

--- position_test_main.py
+++ position_test_main.py
@@ -5,14 +5,14 @@ import time
 
 if __name__ == '__main__':
     arduino = SerialCommunication.SerialCommunication('/dev/SensorActor')
-    mc = MotorControl.MotorControl(com='/dev/Motor')
+    #mc = MotorControl.MotorControl(com='/dev/Motor')
 
     posSensor = PosSensor.PosSensor()
 
     alpha_ = 2000
     beta_ = 2000
 
-    mc.drive_x(20, 10000)
+    #mc.drive_x(20, 10000)
     while 1:
         time.sleep(0.5)
         battery_v = arduino.getBatteryVoltage()
@@ -21,8 +21,10 @@ if __name__ == '__main__':
             beta_ = arduino.getRawBeta()
         except:
             pass
-        dist = mc.get_distance_driven()
-        x_dist = 40.0 + dist['x']
+        #dist = mc.get_distance_driven()
+
+        #x_dist = 40.0 + dist['x']
+        x_dist = 192.5
         pos = posSensor.get_pos_prachtstueck(alpha_, beta_, x_dist)
         print('x:{:5.1f}  z:{:5.1f} | s:{:5.1f} | ralpha:{:4d} rbeta:{:4d} | battery:{:5.1f}'.format(pos.x, pos.z, x_dist, alpha_, beta_, battery_v))
 
