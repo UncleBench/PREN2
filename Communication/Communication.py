778b907d80588ddd3efe1f06339ec5608d869ce1
Aron <schmied.aron@gmail.com>
2018-05-10 14:17:51 +0200

    changed import

--- Communication/Communication.py
+++ Communication/Communication.py
@@ -1,7 +1,7 @@
-from ..MessageQueue.MessageQueue import MessageQueue, Message
+from MessageQueue.MessageQueue import MessageQueue, Message
 from MotorControl import MotorControl
 from SerialCommunication import SerialCommunication
-from ..PositionDetermination.PosSensor import PosSensor
+from PositionDetermination.PosSensor import PosSensor
 from multiprocessing import Process
 from threading import Thread, RLock
 from time import sleep
@@ -19,9 +19,10 @@ class Communication():
         self.main_queue = MessageQueue(callback=self.command_interpreter, quname='')
         self.receiver_queue = MessageQueue(quname='')
         self.position_thread = Thread(target=self.update_position)
-        self.sens_act = SerialCommunication(com=self.sens_act_com)
-        self.motor = MotorControl(com=self.motor_com)
         self.pos_sensor = PosSensor()
+        #self.sens_act = SerialCommunication(com=self.sens_act_com)
+        #self.motor = MotorControl(com=self.motor_com)
+        #self.position_thread.start()
 
     def update_position(self):
         while True:
