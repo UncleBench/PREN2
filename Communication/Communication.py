fb473d16fa1525bfcfc25a1d1233fb8b548efc88
Aron <schmied.aron@gmail.com>
2018-05-10 17:33:37 +0200

    removed some prints

--- Communication/Communication.py
+++ Communication/Communication.py
@@ -23,7 +23,7 @@ class Communication():
         #self.main_queue = MessageQueue(callback=self.command_interpreter, qname='main')
         self.main_queue = MessageQueue(callback=None, qname='main')
         #self.receiver_queue = MessageQueue(qname='')
-        self.position_thread = Thread(target=self.update_position)
+        self.position_thread = Thread(target=self.update_position, name="Position Update")
         self.pos_sensor = PosSensor()
         self.sens_act = SerialCommunication(com=self.sens_act_com)
         self.motor = MotorControl(com=self.motor_com)
@@ -39,21 +39,17 @@ class Communication():
             raw_beta = self.sens_act.getRawBeta()
             battery_voltage = self.sens_act.getBatteryVoltage()
             self.sens_act_lock.release()
-            print "sens act lock passed"
 
             self.motor_lock.acquire()
             #driven_dist = self.motor.get_distance_driven()
             driven_dist = {'x': 20.0, 'z': 20.0}
             self.motor_lock.release()
-            print "motor lock passed"
 
             pos = self.pos_sensor.get_pos_load_by_raw(raw_alpha, raw_beta, driven_dist['x'], driven_dist['z'])
 
             msg = Message("Position", [pos.x, pos.z, battery_voltage])
             #self.receiver_queue.send(msg)
             self.main_queue.send(msg)
-
-            print [pos.x, pos.z, battery_voltage]
 
 
     def command_interpreter(self, command):
