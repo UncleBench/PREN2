a88ee5e26072d2b819dbe5eade31fdcc0cafd4bd
Vali <valentin@pinktree-communications.com>
2018-05-10 13:52:14 +0200

    Added Thread.join after Thread.start to constructor

--- MessageQueue/MessageQueue.py
+++ MessageQueue/MessageQueue.py
@@ -8,9 +8,9 @@ class MessageQueue(object):
         self.sender = Connection('amqp://').SimpleBuffer(self.qname)
         if receive:
             self.receiver = Connection('amqp://').SimpleBuffer(self.qname)
-            self.worker = Thread(target=self.receive, name='MQ'+self.qname,
-                            args=(callback,))
+            self.worker = Thread(target=self.receive, name='MQ'+self.qname, args=(callback,))
             self.worker.start()
+            self.worker.join()
 
     def receive(self, callback):
         while self.active:
