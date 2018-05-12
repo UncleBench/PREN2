6dca14adf5d7045ee9e3c7235575597b99bd6034
David Edelmann <david.edelmann@stud.hslu.ch>
2018-05-12 19:52:19 +0200

    switch to PiCamera

--- test_Vision.py
+++ test_Vision.py
@@ -12,6 +12,6 @@ def interpret_command(msg):
 if __name__ == '__main__':
     main_queue = MessageQueue(callback=interpret_command, qname='ps_main')
     vision_queue = MessageQueue(qname='ps_vision')
-    vision = Vision(usePiCamera=False, debug=False)
+    vision = Vision(usePiCamera=True, debug=False)
     time.sleep(2.0)
     vision_queue.send(Message('start'))
